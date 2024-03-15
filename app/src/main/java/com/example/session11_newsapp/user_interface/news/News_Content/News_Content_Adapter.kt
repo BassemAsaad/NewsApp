3a79d3fd9f50fec49cd139b9ceaf889a73e1e593
BassemAsaad <bassemasaad11@gmail.com>
2024-03-16 01:23:32 +0200

    test

---
+++ app/src/main/java/com/example/session11_newsapp/user_interface/news/News_Content/News_Content_Adapter.kt
@@ -0,0 +1,4 @@
+package com.example.session11_newsapp.User_Interface.News.News_Content
+
+class News_Content_Adapter  {
+}
---
+++ app/src/main/java/com/example/session11_newsapp/user_interface/news/News_Content/News_Full_Content.kt
@@ -0,0 +1,15 @@
+package com.example.session11_newsapp.User_Interface.News.News_Content
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+class News_Full_Content : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
